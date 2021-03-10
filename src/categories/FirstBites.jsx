ed3be14a4878257df5cedd8d247ad293146c0bd9
anthony-bui <nhut.anthony.bui@gmail.com>
2021-03-10 14:29:43 -0600

    Initial layout for a category defined

--- src/categories/FirstBites.jsx
+++ src/categories/FirstBites.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { firstBites } from '../assets/menuData';
 
 const FirstBites = () => {
-  return (
-    <div>
-      First Bite
-    </div>
-  )
-}
+	return (
+		<div className='space-y-4'>
+			{firstBites.map((item) => {
+				return (
+					<div className='md:text-center'>
+						<h1>{item.title}</h1>
+						<p>{item.description}</p>
+						<p>{item.price}</p>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
 
-export default FirstBites
+export default FirstBites;
--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -9,36 +9,56 @@ const MenuPage = () => {
 
 	return (
 		<div className='w-full'>
-			<ul className='text-2xl my-4 w-full flex flex-wrap justify-center'>
-				<li className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800'>
-					<Link to={`${url}/firstbites`}>First Bites</Link>
-				</li>
-				<li className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800'>
-					<Link to={`${url}/soups`}>Soups</Link>
-				</li>
-        <li className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800'>
-					<Link to={`${url}/refreshingdishes`}>Refreshing Dishes</Link>
-				</li>
-        <li className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800'>
-					<Link to={`${url}/friedrice`}>Fried Rice</Link>
-				</li>
-        <li className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800'>
-					<Link to={`${url}/curry`}>Curry</Link>
-				</li>
-        <li className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800'>
-					<Link to={`${url}/noodles`}>Noodles</Link>
-				</li>
-        <li className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800'>
-					<Link to={`${url}/stirfry`}>Stir Fry</Link>
-				</li>
-        <li className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800'>
-					<Link to={`${url}/classicdishes`}>Titaya's Classic Dishes</Link>
-				</li>
-        <li className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800'>
-					<Link to={`${url}/beveragesanddesserts`}>Beverages & Desserts</Link>
-				</li>
-			</ul>
-			<div className='my-12'>
+			<div className='text-2xl mt-8 w-full flex flex-wrap justify-center'>
+				<Link
+					className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800'
+					to={`${url}/firstbites`}
+				>
+					First Bites
+				</Link>
+
+				<Link className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800' to={`${url}/soups`}>
+					Soups
+				</Link>
+
+				<Link
+					className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800'
+					to={`${url}/refreshingdishes`}
+				>
+					Refreshing Dishes
+				</Link>
+
+				<Link className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800' to={`${url}/friedrice`}>
+					Fried Rice
+				</Link>
+
+				<Link className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800' to={`${url}/curry`}>
+					Curry
+				</Link>
+
+				<Link className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800' to={`${url}/noodles`}>
+					Noodles
+				</Link>
+
+				<Link className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800' to={`${url}/stirfry`}>
+					Stir Fry
+				</Link>
+
+				<Link
+					className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800'
+					to={`${url}/classicdishes`}
+				>
+					Titaya's Classic Dishes
+				</Link>
+
+				<Link
+					className='border-2 m-2 px-4 py-2 rounded-lg border-gray-800'
+					to={`${url}/beveragesanddesserts`}
+				>
+					Beverages & Desserts
+				</Link>
+			</div>
+			<div className='my-12 px-4'>
 				<Switch>
 					<Route exact path={`${path}/firstbites`} component={FirstBites} />
 					<Route exact path={`${path}/soups`} component={Soups} />
